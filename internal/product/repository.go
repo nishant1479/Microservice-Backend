b1b09d398b490ad4ffba17202c80a2724d981af2
nishant1479 <nishantdubey147@gmail.com>
2025-08-22 23:26:48 +0530

    feat: interface for services

---
+++ internal/product/repository.go
@@ -0,0 +1,23 @@
+package product
+
+import (
+	"context"
+
+	"github.com/nishant1479/Microservice-Backend/internal/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// MongoRepository Product
+type MongoRepository interface {
+	Create(ctx context.Context, product *models.Product) (*models.Product, error)
+	Update(ctx context.Context, product *models.Product) (*models.Product, error)
+	GetByID(ctx context.Context, productID primitive.ObjectID) (*models.Product, error)
+	Search(ctx context.Context, search string, pagination *utils.Pagination) (*models.ProductsList, error)
+}
+
+// RedisRepository Product
+type RedisRepository interface {
+	SetProduct(ctx context.Context, product *models.Product) error
+	GetProductByID(ctx context.Context, productID primitive.ObjectID) (*models.Product, error)
+	DeleteProduct(ctx context.Context, productID primitive.ObjectID) error
+}
