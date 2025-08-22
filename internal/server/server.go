cdd2ecc22b90ab4940347b7834842091125938e9
nishant1479 <nishantdubey147@gmail.com>
2025-08-22 21:38:28 +0530

    feat: grpc Middleware

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,19 @@ package server
 
 import (
 	"context"
+	"net"
+	"strings"
+	"time"
+
+
+
+	
+	// gRPC middleware
+	grpcrecovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	grpc_opentracing "github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
@@ -10,6 +23,9 @@ import (
 	"github.com/opentracing/opentracing-go"
 	"github.com/redis/go-redis"
 	"go.mongodb.org/mongo-driver/mongo"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/protobuf/internal/errors"
 )
 
 type server struct {
@@ -49,6 +65,41 @@ func (s *server) Run() error{
 	
 	im := interceptors.NewInterceptorManger(s.log,s.cfg)
 	mw := middleware.NewMiddlewareManger(s.log,s.cfg)
+
+	grpcAddr := s.cfg.Server.Port
+	if !strings.HasPrefix(grpcAddr,":") {	
+		grpcAddr = ":" + grpcAddr
+	}
+	listener, err := net.Listen("tcp",grpcAddr)
+	if err != nil {
+		return errors.Wrap(err,"net.Listen")
+	}
+
+	gppcServer := grpc.NewServer(
+		grpc.KeepaliveParams(keepalive.ServerParameters{
+			MaxConnectionIdle: s.cfg.Server.MaxConnectionIdle*time.Minute,
+			Timeout: 			s.cfg.Server.Timeout*time.Second,
+			MaxConnectionAge: 	s.cfg.Server.MaxConnectionAge*time.Minute,
+			Time: 				s.cfg.Server.Timeout*time.Minute,
+		}),
+		grpc.ChainUnaryInterceptor(
+			grpc_ctxtags.UnaryServerInterceptor(),
+			grpc_opentracing.UnaryServerInterceptor(),
+			grpc_prometheus.UnaryServerInterceptor(),
+			grpcrecovery.UnaryServerInterceptor(),
+			im.Logger,
+		),
+	)
+	
+	prodSvc := productGrpc.NewProductService(s.log,productUC,validate)
+	productsService.RegisterProductsServiceServer(grpcServer,prodSvc)
+	grpc_prometheus.Register(grpcServer)
+
+	v1 := s.echo.Group("/api/v1")
+	v1.Use(mw.Metrics)
+
+	productHandlers := productsHttpV1.newProductHandlers(s.log,productUC,validate,v1.Group("/products"),mw)
+	productHandlers.MapRoutes()
 
 	
 	return nil
